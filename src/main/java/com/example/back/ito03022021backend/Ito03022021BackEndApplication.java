306e25318a79fb24876c3851d49a69c37398bd42
Peeter Tarvas <peetertarvas@gmail.com>
2021-09-04 12:07:19 +0300

    initial commit

---
+++ src/main/java/com/example/back/ito03022021backend/Ito03022021BackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.back.ito03022021backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ito03022021BackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ito03022021BackEndApplication.class, args);
+    }
+
+}
