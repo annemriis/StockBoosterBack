ee162fd3b2e67bc58858c852b9ba3a6ee610f5b4
Peeter Tarvas <peetertarvas@gmail.com>
2021-09-13 16:38:08 +0300

    Made methods that set the time range on external api

--- src/main/java/com/example/back/ito03022021backend/api/APIImport.java
+++ src/main/java/com/example/back/ito03022021backend/api/APIImport.java
@@ -11,8 +11,11 @@ public class APIImport {
 
 
     /**
-    This function needs to be called, so that the external API will be connected to this object
+    This function needs to be called,
+     so that the external API will be connected to this object
+     The time on api needs to be set by calling specific methods.
      */
+    // https://github.com/crazzyghost/alphavantage-java
     public void initialize() {
         Config cfg = Config.builder()
                 .key("QL2P2SF7O5SU8LD3")
@@ -24,5 +27,26 @@ public class APIImport {
 
     public AlphaVantage getAlphaVantage() {
         return alphaVantage;
+    }
+
+    /**
+     * Set apis calling to daily
+     */
+    public void setAPIToDaily() {
+        this.alphaVantage.timeSeries().daily();
+    }
+
+    /**
+     * Set apis calling to weekly
+     */
+    public void setAPIToWeekly() {
+        this.alphaVantage.timeSeries().weekly();
+    }
+
+    /**
+     * Set apis calling to intraday
+     */
+    public void setAPIToIntraday() {
+        this.alphaVantage.timeSeries().intraday();
     }
 }
