6516b9d12753deec075d0cb7e7f5c4ca73eb51fc
PeeterTarvas <peetertarvas@gmail.com>
2021-09-20 13:27:28 +0300

    Added user entity

--- src/main/java/com/example/back/ito03022021backend/model/User.java
+++ src/main/java/com/example/back/ito03022021backend/model/User.java
@@ -19,6 +19,22 @@ public class User {
     public User() {
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public Long getId() {
         return id;
     }
