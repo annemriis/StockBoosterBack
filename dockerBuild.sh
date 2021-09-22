573c93a86e010c574676f755655fc091835d5445
PeeterTarvas <peetertarvas@gmail.com>
2021-09-22 22:29:42 +0300

    i think Im close to docker compose ready

--- dockerBuild.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-# shellcheck disable=SC1001
-docker build --build-arg JAR_FILE=C:\Users\Admin\IdeaProjects\ito0302-2021-back-end\build\libs\ito0302-2021-back-end-0.0.1.jar -t springio/gs-spring-boot-docker .
--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env sh
-./gradlew clean build -x test && java -jar build/libs/gs-spring-boot-docker-0.1.0.jar
+# This installs docker and other stuff needed for this program to work
+
+sudo gradle clean && gradle build -x test && java -jar build/libs/gs-spring-boot-docker-0.1.0.jar
 
