e32583844add928f088602327a13ea9f1cc0fd34
Annemari <ariisi@ttu.ee>
2021-09-27 09:26:08 +0300

    ApiController sends StockDto to the front-end.

--- src/main/java/com/example/back/ito03022021backend/contorllers/ApiController.java
+++ src/main/java/com/example/back/ito03022021backend/contorllers/ApiController.java
@@ -2,7 +2,9 @@ package com.example.back.ito03022021backend.contorllers;
 
 
 import com.crazzyghost.alphavantage.timeseries.response.StockUnit;
+import com.example.back.ito03022021backend.dto.StockDto;
 import com.example.back.ito03022021backend.services.api.ApiService;
+import com.example.back.ito03022021backend.services.api.StockSendingService;
 import com.example.back.ito03022021backend.services.api.StockService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -15,19 +17,28 @@ public class ApiController {
 
     private final ApiService api;
     private final StockService stockService;
+    private final StockSendingService stockSendingService;
 
     @Autowired
-    public ApiController(ApiService api, StockService stockService) {
+    public ApiController(ApiService api, StockService stockService, StockSendingService stockSendingService) {
         this.api = api;
         this.api.setApiToIntraday();
         this.stockService = stockService;
+        this.stockSendingService = stockSendingService;
     }
     // https://gitlab.cs.ttu.ee/petarv/iti0302-2021-heroes-back/-/tree/feature/api-w-db/src/main
 
     @GetMapping(path = "/stock/{symbol}")
-    public List<StockUnit> getStock(@PathVariable String symbol) {
-        return this.stockService.getStockDaily(symbol);
+    public StockDto getStock(@PathVariable String symbol) {
+        return this.stockSendingService.getStockDaily(symbol);
     }
+
+    /**
+     * @GetMapping(path = "/stock/{symbol}")
+     * public List<StockUnit> getStock(@PathVariable String symbol) {
+     *   return this.stockService.getStockDaily(symbol);
+     * }
+     */
 
 
 
