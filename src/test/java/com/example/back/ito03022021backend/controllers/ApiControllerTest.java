0765e4beae166fc4a4dda0ce1acc0e29ac7935d8
Annemari <ariisi@ttu.ee>
2021-09-28 22:45:48 +0300

    Tests for ApiController, StockSendingService and StockServiceTest.

--- src/test/java/com/example/back/ito03022021backend/controllers/ApiControllerTest.java
+++ src/test/java/com/example/back/ito03022021backend/controllers/ApiControllerTest.java
@@ -11,7 +11,11 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -33,6 +37,21 @@ public class ApiControllerTest {
         StockDto stockDto = objectMapper.readValue(contentAsString, new TypeReference<>() {});
         assertEquals(StockDto.class, stockDto.getClass());
         assertEquals("GOOG", stockDto.getSymbol());
+
+        // Current date.
+        Date date = new Date();
+        String modifiedDate = new SimpleDateFormat("yyyy-MM-dd").format(date);
+        int day = Integer.parseInt(modifiedDate.substring(8));
+        int month = Integer.parseInt(modifiedDate.substring(5, 7));
+        int year = Integer.parseInt(modifiedDate.substring(0, 4));
+
+        // Test day, month and year.
+        assertTrue(day == Integer.parseInt(stockDto.getLastDate().substring(8))  // Day.
+                || day - 1 == Integer.parseInt(stockDto.getLastDate().substring(8))
+                || day - 2 == Integer.parseInt(stockDto.getLastDate().substring(8)));
+        assertTrue(month == Integer.parseInt(stockDto.getLastDate().substring(5, 7))  // Month.
+                || month - 1 == Integer.parseInt(stockDto.getLastDate().substring(5, 7)));
+        assertEquals(year, Integer.parseInt(stockDto.getLastDate().substring(0, 4)));  // Year.
     }
 
 
--- src/test/java/com/example/back/ito03022021backend/services/api/StockServiceTest.java
+++ src/test/java/com/example/back/ito03022021backend/services/api/StockServiceTest.java
@@ -1,4 +1,70 @@
 package com.example.back.ito03022021backend.services.api;
 
+import com.crazzyghost.alphavantage.timeseries.response.StockUnit;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
 public class StockServiceTest {
+
+    private ApiService apiService = new ApiService();
+    private StockService stockService = new StockService(apiService);
+
+    @Test
+    void filterStockByDateOneMonthFiltersListWithStockUnits() {
+        List<StockUnit> stockUnits = stockService.getStockDaily("TWTR");
+        assertEquals(100, stockUnits.size());
+
+        // Current date.
+        Date date = new Date();
+        String modifiedDate = new SimpleDateFormat("yyyy-MM-dd").format(date);
+        int day = Integer.parseInt(modifiedDate.substring(8));
+        int month = Integer.parseInt(modifiedDate.substring(5, 7));
+        int year = Integer.parseInt(modifiedDate.substring(0, 4));
+
+        List<StockUnit> filteredStockUnits = stockService.filterStockByDateOneMonth(stockUnits, day, month, year);
+        assertTrue(30 > filteredStockUnits.size());
+        assertTrue(20 <= filteredStockUnits.size());
+
+        StockUnit stockUnit = stockUnits.get(0);
+        assertEquals(StockUnit.class, stockUnit.getClass());
+        // Test day, month and year.
+        assertTrue(day == Integer.parseInt(stockUnit.getDate().substring(8))
+                || day - 1 == Integer.parseInt(stockUnit.getDate().substring(8))
+                || day - 2 == Integer.parseInt(stockUnit.getDate().substring(8)));
+        assertTrue(month == Integer.parseInt(stockUnit.getDate().substring(5, 7))
+                || month - 1 == Integer.parseInt(stockUnit.getDate().substring(5, 7)));
+        assertEquals(year, Integer.parseInt(stockUnit.getDate().substring(0, 4)));
+    }
+
+    @Test
+    void getStockDailyWithTimePeriodOneMonthReturnsListWithStockUnits() {
+        List<StockUnit> stockUnits = stockService.getStockDailyWithTimePeriodOneMonth("GOOG");
+        assertTrue(30 > stockUnits.size());
+        assertTrue(20 <= stockUnits.size());
+        StockUnit stockUnit = stockUnits.get(0);
+        assertEquals(StockUnit.class, stockUnit.getClass());
+
+        // Current date.
+        Date date = new Date();
+        String modifiedDate = new SimpleDateFormat("yyyy-MM-dd").format(date);
+        int day = Integer.parseInt(modifiedDate.substring(8));
+        int month = Integer.parseInt(modifiedDate.substring(5, 7));
+        int year = Integer.parseInt(modifiedDate.substring(0, 4));
+
+        // Test day, month and year.
+        assertTrue(day == Integer.parseInt(stockUnit.getDate().substring(8))
+                || day - 1 == Integer.parseInt(stockUnit.getDate().substring(8))
+                || day - 2 == Integer.parseInt(stockUnit.getDate().substring(8)));
+        assertTrue(month == Integer.parseInt(stockUnit.getDate().substring(5, 7))
+                || month - 1 == Integer.parseInt(stockUnit.getDate().substring(5, 7)));
+        assertEquals(year, Integer.parseInt(stockUnit.getDate().substring(0, 4)));
+    }
 }
