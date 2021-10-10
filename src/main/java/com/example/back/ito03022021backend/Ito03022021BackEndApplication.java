66403b5c9cf0b5ff0dca1c7526192b1942777082
Annemari Riisimäe <ariisi@ttu.ee>
2021-10-10 10:58:09 +0300

    Back-end initialises Stock database. Class InitialiseStockDatabase for Stock database initialization.

--- src/main/java/com/example/back/ito03022021backend/Ito03022021BackEndApplication.java
+++ src/main/java/com/example/back/ito03022021backend/Ito03022021BackEndApplication.java
@@ -1,8 +1,18 @@
 package com.example.back.ito03022021backend;
 
+import com.example.back.ito03022021backend.initialise.InitialiseStockDatabase;
+import com.example.back.ito03022021backend.model.StockRepository;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.util.FileCopyUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 @EntityScan
 @SpringBootApplication
@@ -12,4 +22,20 @@ public class Ito03022021BackEndApplication {
         SpringApplication.run(Ito03022021BackEndApplication.class, args);
     }
 
+    // Meetod on hetkel vales kohas ja tagastab midagi.
+    @Bean
+    public ApplicationRunner init(StockRepository repository) {
+        InitialiseStockDatabase initialiseStockDatabase = new InitialiseStockDatabase();
+
+        try {
+            InputStream is = new ClassPathResource("/symbols/symbols_list.txt").getInputStream();
+            String contents = new String(FileCopyUtils.copyToByteArray(is), StandardCharsets.UTF_8);
+            initialiseStockDatabase.initialiseStockDatabase(repository, contents);
+            is.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return args -> repository.findAll().forEach(stock -> System.out.println(stock.getSymbol()));
+    }
 }
---
+++ src/main/java/com/example/back/ito03022021backend/initialise/InitialiseStockDatabase.java
@@ -0,0 +1,50 @@
+package com.example.back.ito03022021backend.initialise;
+
+import com.crazzyghost.alphavantage.timeseries.response.StockUnit;
+import com.example.back.ito03022021backend.builders.StockBuilder;
+import com.example.back.ito03022021backend.model.Stock;
+import com.example.back.ito03022021backend.model.StockRepository;
+import com.example.back.ito03022021backend.services.api.ApiService;
+import com.example.back.ito03022021backend.services.api.StockService;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static java.lang.Thread.sleep;
+
+public class InitialiseStockDatabase {
+
+    private ApiService apiService = new ApiService();
+    private StockService stockService = new StockService(apiService);
+
+    public void initialiseStockDatabase(StockRepository repository, String contents) {
+        List<String> symbols = Arrays.asList(contents.split(", "));
+        addStocksToDatabase(symbols, repository);
+    }
+
+    public void addStocksToDatabase(List<String> symbols, StockRepository stockRepository) {
+        for (int i = 0; i < symbols.size(); i++) {
+            String symbol = symbols.get(i);
+            List<StockUnit> stockUnits = stockService.getStockDaily(symbol);
+            if (stockUnits.size() > 1) {
+                // Get data for stock.
+                StockUnit stockUnit1 = stockUnits.get(0);
+                StockUnit stockUnit2 = stockUnits.get(1);
+                Double lastClose = stockUnit2.getClose();
+                Double close = stockUnit1.getClose();
+                // Create new Stock instance.
+                Stock stock = new StockBuilder()
+                        .withSymbol(symbol)
+                        .withLastClose(lastClose)
+                        .withClose(close)
+                        .buildStock();
+                stockRepository.save(stock);
+            }
+            try {
+                sleep(10000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
