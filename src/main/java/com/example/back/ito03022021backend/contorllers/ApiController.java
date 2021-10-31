7592eb659ade9f18e121bf26cc6b549ae39f9c6f
Annemari Riisimäe <ariisi@ttu.ee>
2021-10-31 12:57:23 +0200

    Added restart for stocks service in .gitlab-ci.yml. Added test path in the ApiController class will remove it in the next commit.

--- src/main/java/com/example/back/ito03022021backend/contorllers/ApiController.java
+++ src/main/java/com/example/back/ito03022021backend/contorllers/ApiController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@RequestMapping(path = "/api")
+@RequestMapping(path = {"/api", "/api2"})
 @RestController
 public class ApiController {
 
