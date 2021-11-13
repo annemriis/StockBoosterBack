9bd79ea751b0d545d52e884d75329af431d0474e
Annemari <ariisi@ttu.ee>
2021-11-13 20:34:16 +0200

    Changed InitialiseStockDatabaseTest.

--- src/test/java/com/example/back/ito03022021backend/initialise/InitialiseStockDatabaseTest.java
+++ src/test/java/com/example/back/ito03022021backend/initialise/InitialiseStockDatabaseTest.java
@@ -33,7 +33,31 @@ public class InitialiseStockDatabaseTest {
                 .withStockCloseInfo(List.of(192.4, 293.4, 176.2))
                 .withSymbol("AAPL")
                 .buildStockDto();
+        StockDto GOOGStockDto = new StockDtoBuilder()
+                .withClose(132.4)
+                .withStockCloseInfo(List.of(132.4, 2923.4, 1736.2))
+                .withSymbol("GOOG")
+                .buildStockDto();
+        StockDto MMMStockDto = new StockDtoBuilder()
+                .withClose(1932.4)
+                .withStockCloseInfo(List.of(1932.4, 2293.4, 1736.2))
+                .withSymbol("MMM")
+                .buildStockDto();
+        StockDto ABTStockDto = new StockDtoBuilder()
+                .withClose(1932.4)
+                .withStockCloseInfo(List.of(1932.4, 2943.4, 1276.2))
+                .withSymbol("ABT")
+                .buildStockDto();
+        StockDto ABBVStockDto = new StockDtoBuilder()
+                .withClose(192.4)
+                .withStockCloseInfo(List.of(192.4, 2933.4, 176.2))
+                .withSymbol("ABBV")
+                .buildStockDto();
         when(stockSendingService.getStockDaily("AAPL")).thenReturn(Optional.of(AAPLStockDto));
+        when(stockSendingService.getStockDaily("GOOG")).thenReturn(Optional.of(GOOGStockDto));
+        when(stockSendingService.getStockDaily("MMM")).thenReturn(Optional.of(MMMStockDto));
+        when(stockSendingService.getStockDaily("ABT")).thenReturn(Optional.of(ABTStockDto));
+        when(stockSendingService.getStockDaily("ABBV")).thenReturn(Optional.of(ABBVStockDto));
         List<String> symbols = List.of("AAPL", "GOOG", "MMM", "ABT", "ABBV");
         ApiService apiService = new ApiService();
         apiService.initialiseApiService(System.getProperty("api.key"));
@@ -45,6 +69,10 @@ public class InitialiseStockDatabaseTest {
         // Stocks are in the database.
         assertNotNull(stockRepository.findStockBySymbol("AAPL"));
         assertEquals("AAPL", stockRepository.findStockBySymbol("AAPL").getSymbol());
+        assertEquals("GOOG", stockRepository.findStockBySymbol("GOOG").getSymbol());
+        assertEquals("MMM", stockRepository.findStockBySymbol("MMM").getSymbol());
+        assertEquals("ABT", stockRepository.findStockBySymbol("ABT").getSymbol());
+        assertEquals("ABBV", stockRepository.findStockBySymbol("ABBV").getSymbol());
 
         // Close and lastClose are in the database.
         assertEquals(AAPLStockDto.getStockCloseInfo().get(1), stockRepository.findStockBySymbol("AAPL").getLastClose());
@@ -59,7 +87,31 @@ public class InitialiseStockDatabaseTest {
                 .withStockCloseInfo(List.of(234.5, 345.5, 123.5))
                 .withSymbol("GOOG")
                 .buildStockDto();
+        StockDto AAPLStockDto = new StockDtoBuilder()
+                .withClose(192.4)
+                .withStockCloseInfo(List.of(192.4, 293.4, 176.2))
+                .withSymbol("AAPL")
+                .buildStockDto();
+        StockDto MMMStockDto = new StockDtoBuilder()
+                .withClose(192.4)
+                .withStockCloseInfo(List.of(192.4, 2393.4, 176.2))
+                .withSymbol("MMM")
+                .buildStockDto();
+        StockDto ABTStockDto = new StockDtoBuilder()
+                .withClose(192.4)
+                .withStockCloseInfo(List.of(192.4, 2393.4, 176.2))
+                .withSymbol("ABT")
+                .buildStockDto();
+        StockDto ABBVStockDto = new StockDtoBuilder()
+                .withClose(1922.4)
+                .withStockCloseInfo(List.of(1922.4, 293.4, 176.2))
+                .withSymbol("ABBV")
+                .buildStockDto();
         when(stockSendingService.getStockDaily("GOOG")).thenReturn(Optional.of(GOOGStockDto));
+        when(stockSendingService.getStockDaily("AAPL")).thenReturn(Optional.of(AAPLStockDto));
+        when(stockSendingService.getStockDaily("MMM")).thenReturn(Optional.of(MMMStockDto));
+        when(stockSendingService.getStockDaily("ABT")).thenReturn(Optional.of(ABTStockDto));
+        when(stockSendingService.getStockDaily("ABBV")).thenReturn(Optional.of(ABBVStockDto));
         String contents = "AAPL, GOOG, MMM, ABT, ABBV";
 
         ApiService apiService = new ApiService();
@@ -70,8 +122,12 @@ public class InitialiseStockDatabaseTest {
         initialiseStockDatabase.initialiseStockDatabase(stockRepository, contents);
 
         // Stocks are in the database.
-        assertNotNull(stockRepository.findStockBySymbol("MMM"));
+        assertNotNull(stockRepository.findStockBySymbol("AAPL"));
+        assertEquals("AAPL", stockRepository.findStockBySymbol("AAPL").getSymbol());
+        assertEquals("GOOG", stockRepository.findStockBySymbol("GOOG").getSymbol());
         assertEquals("MMM", stockRepository.findStockBySymbol("MMM").getSymbol());
+        assertEquals("ABT", stockRepository.findStockBySymbol("ABT").getSymbol());
+        assertEquals("ABBV", stockRepository.findStockBySymbol("ABBV").getSymbol());
 
         // Close and lastClose are in the database.
         assertEquals(GOOGStockDto.getStockCloseInfo().get(1), stockRepository.findStockBySymbol("GOOG").getLastClose());
