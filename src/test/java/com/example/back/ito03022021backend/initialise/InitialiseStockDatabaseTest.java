3a6ef89fc3676200121944bf76b6219d89e367bb
Annemari <ariisi@ttu.ee>
2021-11-13 19:42:26 +0200

    Added tests.

---
+++ src/test/java/com/example/back/ito03022021backend/initialise/InitialiseStockDatabaseTest.java
@@ -0,0 +1,86 @@
+package com.example.back.ito03022021backend.initialise;
+
+import com.example.back.ito03022021backend.dto.StockDto;
+import com.example.back.ito03022021backend.dto.StockDtoBuilder;
+import com.example.back.ito03022021backend.repositories.StockRepository;
+import com.example.back.ito03022021backend.services.api.ApiService;
+import com.example.back.ito03022021backend.services.api.StockSendingService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class InitialiseStockDatabaseTest {
+
+    @Autowired
+    private StockRepository stockRepository;
+
+    @Test
+    void testAddStockToDatabase() {
+        StockSendingService stockSendingService = Mockito.mock(StockSendingService.class);
+        StockDto AAPLStockDto = new StockDtoBuilder()
+                .withClose(192.4)
+                .withStockCloseInfo(List.of(192.4, 293.4, 176.2))
+                .withSymbol("AAPL")
+                .buildStockDto();
+        when(stockSendingService.getStockDaily("AAPL")).thenReturn(Optional.of(AAPLStockDto));
+        List<String> symbols = List.of("AAPL", "GOOG", "MMM", "ABT", "ABBV");
+        ApiService apiService = new ApiService();
+        apiService.initialiseApiService(System.getProperty("api.key"));
+        InitialiseStockDatabase initialiseStockDatabase = new InitialiseStockDatabase(apiService);
+        initialiseStockDatabase.setStockSendingService(stockSendingService);
+
+        initialiseStockDatabase.addStocksToDatabase(symbols, stockRepository);
+
+        // Stocks are in the database.
+        assertNotNull(stockRepository.findStockBySymbol("AAPL"));
+        assertEquals("AAPL", stockRepository.findStockBySymbol("AAPL").getSymbol());
+        assertEquals("GOOG", stockRepository.findStockBySymbol("GOOG").getSymbol());
+        assertEquals("MMM", stockRepository.findStockBySymbol("MMM").getSymbol());
+        assertEquals("ABT", stockRepository.findStockBySymbol("ABT").getSymbol());
+        assertEquals("ABBV", stockRepository.findStockBySymbol("ABBV").getSymbol());
+
+        // Close and lastClose are in the database.
+        assertEquals(AAPLStockDto.getStockCloseInfo().get(1), stockRepository.findStockBySymbol("AAPL").getLastClose());
+        assertEquals(AAPLStockDto.getClose(), stockRepository.findStockBySymbol("AAPL").getClose());
+    }
+
+    @Test
+    void testInitialiseStockDatabase() {
+        StockSendingService stockSendingService = Mockito.mock(StockSendingService.class);
+        StockDto GOOGStockDto = new StockDtoBuilder()
+                .withClose(234.5)
+                .withStockCloseInfo(List.of(234.5, 345.5, 123.5))
+                .withSymbol("GOOG")
+                .buildStockDto();
+        when(stockSendingService.getStockDaily("GOOG")).thenReturn(Optional.of(GOOGStockDto));
+        String contents = "AAPL, GOOG, MMM, ABT, ABBV";
+
+        ApiService apiService = new ApiService();
+        apiService.initialiseApiService(System.getProperty("api.key"));
+        InitialiseStockDatabase initialiseStockDatabase = new InitialiseStockDatabase(apiService);
+        initialiseStockDatabase.setStockSendingService(stockSendingService);
+
+        initialiseStockDatabase.initialiseStockDatabase(stockRepository, contents);
+
+        // Stocks are in the database.
+        assertNotNull(stockRepository.findStockBySymbol("AAPL"));
+        assertEquals("AAPL", stockRepository.findStockBySymbol("AAPL").getSymbol());
+        assertEquals("GOOG", stockRepository.findStockBySymbol("GOOG").getSymbol());
+        assertEquals("MMM", stockRepository.findStockBySymbol("MMM").getSymbol());
+        assertEquals("ABT", stockRepository.findStockBySymbol("ABT").getSymbol());
+        assertEquals("ABBV", stockRepository.findStockBySymbol("ABBV").getSymbol());
+
+        // Close and lastClose are in the database.
+        assertEquals(GOOGStockDto.getStockCloseInfo().get(1), stockRepository.findStockBySymbol("GOOG").getLastClose());
+        assertEquals(GOOGStockDto.getClose(), stockRepository.findStockBySymbol("GOOG").getClose());
+    }
+}
---
+++ src/test/java/com/example/back/ito03022021backend/services/api/StockCalculationsServiceTest.java
@@ -0,0 +1,106 @@
+package com.example.back.ito03022021backend.services.api;
+
+import com.crazzyghost.alphavantage.timeseries.response.StockUnit;
+import com.example.back.ito03022021backend.StockUnitListBuilder;
+import com.example.back.ito03022021backend.dto.StockDto;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class StockCalculationsServiceTest {
+
+    StockCalculations stockCalculations;
+    StockSendingService stockSendingService;
+    StockService stockService;
+
+    @Autowired
+    public StockCalculationsServiceTest(StockService stockService, StockSendingService stockSendingService,
+                                        StockCalculations stockCalculations) {
+        this.stockCalculations = stockCalculations;
+        this.stockSendingService = stockSendingService;
+        this.stockService = stockService;
+
+    }
+
+    @Test
+    void testStockCalculationsOnAAPL() {
+        StockService stockService = Mockito.mock(StockService.class);
+        List<StockUnit> stockUnits = null;
+        try {
+            stockUnits = StockUnitListBuilder.readFromFile("AAPL_get_daily");
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+        when(stockService.getStockDailyWithTimePeriodOneMonth("AAPL")).thenReturn(stockUnits);
+        Optional<StockDto> dto = stockSendingService.convertToStockDto("AAPL", stockUnits);
+        List<String> stockDateInfo = new LinkedList<>();
+        List<Double> stockCloseInfo = new LinkedList<>();
+        List<Long> stockVolumesMonthly = new ArrayList<>();
+        Double close = stockUnits.get(0).getClose();
+        Double prevClose = stockUnits.get(1).getClose();
+        for (int i = 0; i < stockUnits.size(); i++) {
+            StockUnit stockUnit = stockUnits.get(i);
+            stockCloseInfo.add(stockUnit.getClose());
+            stockDateInfo.add(stockUnit.getDate());
+            stockVolumesMonthly.add(stockUnit.getVolume());
+        }
+
+        StockDto stockDto = new StockDto();
+        if (dto.isPresent()) {
+            stockDto = dto.get();
+        }
+
+        Double percentageChange = stockCalculations.getDailyPercentageChange(close, prevClose);
+        assertEquals(stockDto.getDailyPercentageChange(), percentageChange);
+
+        Double priceChange = stockCalculations.getDailyPriceChange(close, prevClose);
+        assertEquals(stockDto.getDailyPriceChange(), priceChange);
+
+        double avgPrice = stockCalculations.getMonthlyAveragePrice(stockCloseInfo);
+        assertEquals(avgPrice, stockDto.getAveragePriceMonthly());
+
+        long avgVolume = stockCalculations.getMonthlyAverageTradingVolume(stockVolumesMonthly);
+        assertEquals(avgVolume, stockDto.getAverageVolumeMonthly());
+    }
+
+    @Test
+    void testStockCalculationsOnGOOG() {
+        StockService stockService = Mockito.mock(StockService.class);
+        List<StockUnit> stockUnits = null;
+        try {
+            stockUnits = StockUnitListBuilder.readFromFile("GOOG_get_daily");
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+        when(stockService.getStockDailyWithTimePeriodOneMonth("GOOG")).thenReturn(stockUnits);
+        Optional<StockDto> dto = stockSendingService.convertToStockDto("GOOG", stockUnits);
+        List<String> stockDateInfo = new LinkedList<>();
+        List<Double> stockCloseInfo = new LinkedList<>();
+        List<Long> stockVolumesMonthly = new ArrayList<>();
+        for (int i = 0; i < stockUnits.size(); i++) {
+            StockUnit stockUnit = stockUnits.get(i);
+            stockCloseInfo.add(stockUnit.getClose());
+            stockDateInfo.add(stockUnit.getDate());
+            stockVolumesMonthly.add(stockUnit.getVolume());
+        }
+
+        StockDto stockDto = new StockDto();
+        if (dto.isPresent()) {
+            stockDto = dto.get();
+        }
+
+        double avgPrice = stockCalculations.getMonthlyAveragePrice(stockCloseInfo);
+        assertEquals(avgPrice, stockDto.getAveragePriceMonthly());
+
+        long avgVolume = stockCalculations.getMonthlyAverageTradingVolume(stockVolumesMonthly);
+        assertEquals(avgVolume, stockDto.getAverageVolumeMonthly());
+    }
+}
