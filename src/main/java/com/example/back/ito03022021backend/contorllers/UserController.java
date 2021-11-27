22d991691bb540fc0cc10cfd814d87291d9a360d
Peeter Tarvas <peetertarvas@gmail.com>
2021-11-27 19:43:39 +0200

    added security imports, created class that manages Security config and updated UserController

--- src/main/java/com/example/back/ito03022021backend/contorllers/UserController.java
+++ src/main/java/com/example/back/ito03022021backend/contorllers/UserController.java
@@ -4,6 +4,7 @@ package com.example.back.ito03022021backend.contorllers;
 import com.example.back.ito03022021backend.builders.UserBuilder;
 import com.example.back.ito03022021backend.model.User;
 import com.example.back.ito03022021backend.repositories.UsersRepository;
+import com.example.back.ito03022021backend.services.api.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -15,22 +16,18 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     private final UsersRepository repository;
+    private final UserService userService;
 
     @Autowired
-    public UserController(UsersRepository usersRepository) {
+    public UserController(UsersRepository usersRepository, UserService userService) {
         this.repository = usersRepository;
+        this.userService = userService;
     }
 
     @PostMapping(path = "/users", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
             produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public ResponseEntity<User> createUser(@RequestBody User newUser) {
-        User user = new UserBuilder()
-                .withName(newUser.getName())
-                .withEmail(newUser.getEmail())
-                .withPassword(newUser.getPassword())
-                .build();
-        User _user = repository.save(user);
-        return new ResponseEntity<>(_user, HttpStatus.CREATED);
+        return userService.createUser(newUser);
     }
 
     @GetMapping(path = "/user/{name}")
