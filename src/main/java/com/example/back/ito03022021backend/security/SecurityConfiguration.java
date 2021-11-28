b8bc207204da11adba4ba3e193d2934adda7b5c7
Peeter Tarvas <peetertarvas@gmail.com>
2021-11-28 19:28:07 +0200

    add UserConfig and set usernames and passwords in application.properties

--- src/main/java/com/example/back/ito03022021backend/security/SecurityConfiguration.java
+++ src/main/java/com/example/back/ito03022021backend/security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.example.back.ito03022021backend.security;
 
+import com.example.back.ito03022021backend.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,6 +18,9 @@ import static com.example.back.ito03022021backend.security.ApplicationRoles.USER
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserConfig userConfig;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -36,12 +40,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-                .withUser("user")
-                .password(passwordEncoder().encode("user")) // Spring Security 5 requires specifying the password storage format
+                .withUser(userConfig.getUserName())
+                .password(passwordEncoder().encode(userConfig.getUserPassword())) // Spring Security 5 requires specifying the password storage format
                 .authorities(USER)
                 .and()
-                .withUser("admin")
-                .password(passwordEncoder().encode("admin")) // Spring Security 5 requires specifying the password storage format
+                .withUser(userConfig.getAdminName())
+                .password(passwordEncoder().encode(userConfig.getAdminPassword())) // Spring Security 5 requires specifying the password storage format
                 .authorities(USER, ADMIN);
     }
 
---
+++ src/main/java/com/example/back/ito03022021backend/security/UserConfig.java
@@ -0,0 +1,48 @@
+package com.example.back.ito03022021backend.security;
+
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "app.users")
+public class UserConfig {
+
+   private String userName;
+   private String userPassword;
+   private String adminName;
+   private String adminPassword;
+
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getAdminName() {
+        return adminName;
+    }
+
+    public void setAdminName(String adminName) {
+        this.adminName = adminName;
+    }
+
+    public String getAdminPassword() {
+        return adminPassword;
+    }
+
+    public void setAdminPassword(String adminPassword) {
+        this.adminPassword = adminPassword;
+    }
+}
