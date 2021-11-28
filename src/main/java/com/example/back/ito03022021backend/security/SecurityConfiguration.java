ba1cfaf22314d2bf30ae7660538897b425a77cb5
Peeter Tarvas <peetertarvas@gmail.com>
2021-11-28 19:01:21 +0200

    add default user

--- src/main/java/com/example/back/ito03022021backend/security/SecurityConfiguration.java
+++ src/main/java/com/example/back/ito03022021backend/security/SecurityConfiguration.java
@@ -1,18 +1,22 @@
 package com.example.back.ito03022021backend.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
-    public void configure(HttpSecurity http) throws Exception {
+    protected void configure(HttpSecurity http) throws Exception {
         http
                 .httpBasic()
                 .and()
@@ -26,12 +30,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .headers().httpStrictTransportSecurity().disable();
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
                 .withUser("user")
-                .password("user ") // Spring Security 5 requires specifying the password storage format
-                .roles("USER");
+                .password(passwordEncoder().encode("user")) // Spring Security 5 requires specifying the password storage format
+                .authorities("ROLE USER");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
