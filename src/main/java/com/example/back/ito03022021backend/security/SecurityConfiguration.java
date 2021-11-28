3d04fbfcf11dcc0777cb1c281e036c96a6428ef2
Peeter Tarvas <peetertarvas@gmail.com>
2021-11-28 18:44:51 +0200

    make api stateless, disable csrf and strict https

--- src/main/java/com/example/back/ito03022021backend/security/SecurityConfiguration.java
+++ src/main/java/com/example/back/ito03022021backend/security/SecurityConfiguration.java
@@ -1,25 +1,36 @@
-package com.example.back.ito03022021backend.springsecurityauth;
+package com.example.back.ito03022021backend.security;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
+@Configuration
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http
+                .httpBasic()
+                .and()
                 .authorizeRequests()
                 .anyRequest().authenticated()
                 .and()
-                .httpBasic();
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .csrf().disable()
+                .headers().httpStrictTransportSecurity().disable();
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
                 .withUser("user")
-                .password("{noop}pass") // Spring Security 5 requires specifying the password storage format
+                .password("user ") // Spring Security 5 requires specifying the password storage format
                 .roles("USER");
     }
 
