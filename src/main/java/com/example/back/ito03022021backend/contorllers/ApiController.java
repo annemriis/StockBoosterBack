37a88f59d2480358089d9ef0a38f475c480ab97e
Kristjan Variksoo <nutitehnoloogiad@gmail.com>
2021-12-04 13:28:30 +0200

    test commit to check ci/cd

--- src/main/java/com/example/back/ito03022021backend/contorllers/ApiController.java
+++ src/main/java/com/example/back/ito03022021backend/contorllers/ApiController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@RequestMapping(path = "/api")
+@RequestMapping({"/api", "/api2"})
 @RestController
 public class ApiController {
 
